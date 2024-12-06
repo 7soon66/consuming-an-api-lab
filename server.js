e5ed905cff60c0dd54f16c8d4d5d80ef7c932de3
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-12-06 16:12:20 +0300

    excersise done

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const axios = require('axios');
+const ejs = require('ejs');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({
+ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+
+});
+
+app.post('/weather', async (req, res) => {
+  const zipCode = req.body.zipCode;
+  const apiKey = '6f8c1e416d55bf15c53508313ad3e9d3'; 
+
+  try {
+    const response = await axios.get(` api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}`);
+    const weatherData = response.data;
+
+   
+    res.render('weather/show', {
+      city: weatherData.name,
+      temp: weatherData.main.temp,
+      description: weatherData.weather[0].description
+    });
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching weather data');
+  }
+});
+
+app.get('/weather/show', (req, res) => {
+  const { city, temp, description } = req.query;
+  res.render('weather/show', { city, temp, description });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
